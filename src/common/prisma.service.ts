9142d41b3b70dc44134fbb556d7bce95157e6cdb
秦锋 <iamzhoufei@gmail.com>
2023-03-02 10:09:43 +0800

    feat: 修改prisma配置

--- src/common/prisma.service.ts
+++ src/common/prisma.service.ts
@@ -5,6 +5,7 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+    console.log('数据库已连接');
   }
 
   async enableShutdownHooks(app: INestApplication) {
