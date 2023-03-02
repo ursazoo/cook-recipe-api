888c3c250557ea1eb36569938654c32d8ddbe0d6
秦锋 <iamzhoufei@gmail.com>
2023-03-02 10:10:57 +0800

    feat: 修改post module

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { PrismaService } from '../common/prisma.service';
 
 @Module({
-  providers: [PostService],
   controllers: [PostController],
+  providers: [PostService, PrismaService],
+  exports: [PostService],
 })
 export class PostModule {}
