85ba675519a4a0dd42b0625b6af36a548e886ba4
秦锋 <iamzhoufei@gmail.com>
2023-04-02 23:38:37 +0800

    feat: 初始化获取请求ip的服务

--- src/common/request-ip/request-ip.service.ts
+++ src/common/request-ip/request-ip.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import * as geoip from 'geoip-lite';
 
 @Injectable()
-export class RequestIpService {}
+export class RequestIpService {
+  recordIp(request: Request): { data: any } {
+    const realIp =
+      request.headers['x-real-ip'] || request.connection.remoteAddress;
+    const geo = geoip.lookup(realIp);
+    const clientIp = {
+      ip: realIp,
+      region: geo ? geo.region : 'unknown',
+    };
+
+    return {
+      data: clientIp,
+    };
+    // Do some record logic here
+  }
+}
